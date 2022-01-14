5a16ed81c14343fde3b88c3421abc52ce3557ef1
ghalibgendut <ghalibsasmito@gmail.com>
2022-01-14 15:47:53 +0700

    function parameter

--- Latihan-1/function.go
+++ Latihan-1/function.go
@@ -10,14 +10,24 @@ func helloFunc() {
 	fmt.Println("Hello this is a Function after a continue operation!")
 }
 
+func helloUser(firstName string, lastName string) {
+	fmt.Println("Hello", firstName, lastName)
+}
+
 func main() {
-	for i := 0; i < 5; i++ {
-		helloFunction()
+	// for i := 0; i < 5; i++ {
+	// 	helloFunction()
 
-		if i < 3 {
-			continue
-		}
+	// 	if i < 3 {
+	// 		continue
+	// 	}
 
-		helloFunc()
-	}
+	// 	helloFunc()
+	// }
+
+	firstName := "Ujang"
+	lastName := "Jajang"
+	helloUser(firstName, "Budiman")
+	helloUser("Budi", lastName)
+
 }
