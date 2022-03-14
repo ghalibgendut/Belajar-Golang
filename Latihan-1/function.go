97ddea5c560400631ac942d10a213b40463f63d4
ghalibgendut <ghalibsasmito@gmail.com>
2022-03-14 11:30:42 +0700

    return value function in go

--- Latihan-1/function.go
+++ Latihan-1/function.go
@@ -14,6 +14,14 @@ func helloUser(firstName string, lastName string) {
 	fmt.Println("Hello", firstName, lastName)
 }
 
+func returnVal(name string) string {
+	if name == "" {
+		return "Hello, may i know your name?"
+	} else {
+		return "Hello " + name + "!"
+	}
+}
+
 func main() {
 	for i := 0; i < 5; i++ {
 		helloFunction()
@@ -29,5 +37,10 @@ func main() {
 	lastName := "Jajang"
 	helloUser(firstName, "Budiman")
 	helloUser("Budi", lastName)
+
+	result := returnVal("Dodi")
+	fmt.Println(result)
+
+	fmt.Println(returnVal(""))
 
 }
